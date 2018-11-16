050e8f802019cccd243b0492d7362c42593b4581
Shima <shima.jin@icloud.com>
2018-11-16 18:39:31 +0900

    Initial Commit

---
+++ layout-guide-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layout-guide-practice
+//
+//  Created by Jinsei Shima on 2018/11/16.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
